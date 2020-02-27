0e253f285cb7ff0f03602dcfecdfe0bb676cbef2
Michael Braun <57547002+michaelbraunrentonschools@users.noreply.github.com>
2020-02-27 13:59:24 -0800

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,22 @@
+// create an empty array of numbers
+
+let arrayLoop: number[] = []
+
+// create a for loop that increments from 0 through 4
+
+for (let index = 0; index < 4; index++) {
+
+    // return the array loop that multiples that number times 2
+
+    arrayLoop[index] = index *2
+
+    // display the number and the array number 
+
+    game.splash("This multiplies the index times two: " + arrayLoop[...])
+
+}
+
+
+
+
+
